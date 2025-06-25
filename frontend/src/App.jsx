9d9956630e9d1aea1701b5a663acb92d6df254bd
Caleb Calderon <calebc4554@meta.com>
2025-06-24 18:40:15 -0700

    added and installed tailwind and its dependencies, added an example component to test if it works

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,14 @@
 import { useState } from "react";
-import "./App.css";
 
 function App() {
-	return <div>hi</div>;
+	return (
+		<div>
+			// Example component to demonstrate Tailwind CSS
+			<div className="min-h-screen flex items-center justify-center bg-gray-100">
+				<h1 className="text-3xl font-bold text-blue-600">Tailwind is working!</h1>
+			</div>
+		</div>
+	);
 }
 
 export default App;
