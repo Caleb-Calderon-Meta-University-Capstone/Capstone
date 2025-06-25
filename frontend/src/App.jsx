06d08680f92e3c99a2580c1a58259e7fecb68215
Caleb Calderon <calebc4554@meta.com>
2025-06-25 00:18:24 -0700

    added commments for testing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 
 function App() {
 	return (
-		// Example component to demonstrate Tailwind CSS
+		//TODO: Delete -  Example component to demonstrate Tailwind CSS
 		<div>
 			<div className="min-h-screen flex items-center justify-center bg-gray-100">
 				<h1 className="text-3xl font-bold text-blue-600">Tailwind is working!</h1>
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,6 +5,7 @@ import { RouterProvider } from "react-router-dom";
 import { router } from "./router.jsx";
 
 createRoot(document.getElementById("root")).render(
+  //TODO: Delete - Testing React Routing and tailwind
 	<StrictMode>
 		<h1 className="text-center pt-4 text-4xl text-red-300">Test</h1>
 		<RouterProvider router={router} />
