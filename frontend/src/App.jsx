a07f35b48ff579129fc1a3e2d3b1af15d249f179
Caleb Calderon <calebc4554@meta.com>
2025-06-25 00:16:24 -0700

    added react routing for sign up, login, and dashboard

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,8 +2,8 @@ import { useState } from "react";
 
 function App() {
 	return (
+		// Example component to demonstrate Tailwind CSS
 		<div>
-			// Example component to demonstrate Tailwind CSS
 			<div className="min-h-screen flex items-center justify-center bg-gray-100">
 				<h1 className="text-3xl font-bold text-blue-600">Tailwind is working!</h1>
 			</div>
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./router.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-)
+createRoot(document.getElementById("root")).render(
+	<StrictMode>
+		<h1 className="text-center pt-4 text-4xl text-red-300">Test</h1>
+		<RouterProvider router={router} />
+	</StrictMode>
+);
