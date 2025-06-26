3c1aebe2aa72c9b535e0f8df612b894267868461
Caleb Calderon <calebc4554@meta.com>
2025-06-25 17:41:06 -0700

    added private routes for the dashboard

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -8,7 +8,7 @@ const Login = () => {
 	const [loading, setLoading] = useState(false);
 	const [error, setError] = useState(null);
 
-	const { signInUser } = UserAuth(); // Correct method name
+	const { signInUser } = UserAuth(); 
 	const navigate = useNavigate();
 
 	const handleLogin = async (e) => {
