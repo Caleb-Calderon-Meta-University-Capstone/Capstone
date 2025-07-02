1e04561d26be4866af3c2a53b8e2da3c67dc6d1f
Caleb Calderon <calebc4554@meta.com>
2025-07-01 19:48:15 -0700

    removed empty class names, refractored navigation bar ui components with personal styling

--- frontend/src/components/LeaderboardPage.jsx
+++ frontend/src/components/LeaderboardPage.jsx
@@ -3,7 +3,7 @@ import NavigationBar from "./NavigationBar.jsx";
 
 const LeaderboardPage = () => {
 	return (
-		<div className="">
+		<div>
 			<NavigationBar />
 			<div className="bg-blue-400 py-2 px-4 rounded-lg shadow mb-6 w-fit mx-auto border border-gray-300">
 				<h1 className="text-xl font-bold">MICS Leaderboard</h1>
